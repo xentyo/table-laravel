8a34d968130f90bc712a9ad481ebd8930ee1a9f3
Xentyo <xentyo@hotmail.com>
2017-08-03 23:29:58 -0700

    Adding TableServiceProvider

--- src/Providers/TableServiceProvider.php
+++ src/Providers/TableServiceProvider.php
@@ -10,12 +10,7 @@ class TableServiceProvider extends ServiceProvider
 
     public function boot()
     {
-      $this->loadViewsFrom(__DIR__.'/../views', 'tableRender');
-
-      $this->publishes([
-        __DIR__.'/../../views' => base_path('resources/views/vendor/xentyo/table-renderizer'),
-        'tableRender'
-      ]);
+        $this->loadViewsFrom(__DIR__.'/../../views', 'tableRender');
     }
 
     public function register()
